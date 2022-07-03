3b80bdf88758032b93852480117a87f596c6d54d
Qeelro <95707367+Qeelro@users.noreply.github.com>
2022-07-03 10:23:25 +0200

    Update UserController.java

--- src/main/java/com/example/anywhere/controller/UserController.java
+++ src/main/java/com/example/anywhere/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
